e56057673ebd8acff7c05690f77776c0b61d4ead
Eric Johnson <eric.johnson@mycase.com>
2016-11-28 17:32:06 -0800

    emj - HASH -> SHA1

--- push_container.sh
+++ push_container.sh
@@ -7,13 +7,12 @@ configure_aws_cli(){
 
 push_ecr_image(){
     eval $(aws ecr get-login --region us-central-1)
-    docker tag doesntmattertome/app:$CIRCLECI_HASH 133552650589.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLECI_HASH
-    docker push nginx
-    #    docker push 133552650589.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLECI_HASH
-#        docker tag $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1 \
-#                   $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
-#	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
-#	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
+    docker tag doesntmattertome/app:$CIRCLECI_SHA1 $AWS_ACCOUNT.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLECI_SHA1
+    docker push $AWS_ACCOUNT.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLECI_SHA1
+    docker tag $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1 \
+               $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
 }
 
 configure_aws_cli

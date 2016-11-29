f405722e1b892278496c1dbe16507605aeed9db4
Eric Johnson <eric.johnson@mycase.com>
2016-11-28 16:48:58 -0800

    emj - try pushing circleci_hash version directly

--- push_container.sh
+++ push_container.sh
@@ -6,10 +6,12 @@ configure_aws_cli(){
 }
 
 push_ecr_image(){
-	eval $(aws ecr get-login --region us-central-1)
+    eval $(aws ecr get-login --region us-central-1)
+    docker tag doesntmattertome/app:$CIRCLECI_HASH 133552650589.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLECI_HASH
+    docker push doesntmattertome/app:$CIRCLECI_HASH 133552650589.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLECI_HASH
 #        docker tag $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1 \
-                   $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
+#                   $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
+#	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
 #	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
 }
 

dd87c62076a4df765e2c16bf2bf9c4a98a29a2a2
Eric Johnson <eric.johnson@mycase.com>
2016-11-28 20:13:57 -0800

    emj - typo

--- push_container.sh
+++ push_container.sh
@@ -8,7 +8,7 @@ configure_aws_cli(){
 
 push_ecr_image(){
     eval $(aws ecr get-login --region eu-central-1)
-    docker tag doesntmattertome/app:$CIRCLE_SHA1 $AWS_ACCOUNT.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
+    docker tag doesntmattertome/app:$CIRCLE_SHA1 $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
     docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
 #    docker tag $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1 \
 #               $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest

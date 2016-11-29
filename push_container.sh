9070e64af7d49045e3e211af945a7330a9f39e1a
Eric Johnson <eric.johnson@mycase.com>
2016-11-28 16:41:19 -0800

    emj - only try 1 push

--- push_container.sh
+++ push_container.sh
@@ -7,10 +7,10 @@ configure_aws_cli(){
 
 push_ecr_image(){
 	eval $(aws ecr get-login --region us-central-1)
-        docker tag 133552650589.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1 \
-                   133552650589.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
+#        docker tag $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1 \
+                   $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:$CIRCLE_SHA1
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
+#	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/doesntmattertome/app:latest
 }
 
 configure_aws_cli
